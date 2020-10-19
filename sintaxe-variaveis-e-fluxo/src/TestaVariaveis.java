51c4f3df57ac6cf5d5cd14c036c8dc6fbf9a29b3
Warton Barros <warton86@gmail.com>
2020-10-18 21:51:14 -0300

    Treinando Java

---
+++ sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,12 @@
+
+public class TestaVariaveis {
+	
+	public static void main(String[] args) {
+		System.out.println("Olá novo teste!");
+		
+		int idade;
+		idade = 26;
+		System.out.println(idade);
+	}
+
+}
