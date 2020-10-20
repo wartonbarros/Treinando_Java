ff08cca68a236ed9784b78a3a50210f1138084dc
Warton Barros <warton86@gmail.com>
2020-10-20 05:54:09 -0300

    Testando Valores

---
+++ sintaxe-variaveis-e-fluxo/src/TestaValores.java
@@ -0,0 +1,14 @@
+
+public class TestaValores {
+	
+	public static void main(String[] args) {
+		
+		int primeiroNumero = 5;
+		int segundoNumero = 7;
+		segundoNumero = primeiroNumero;
+		primeiroNumero = 10;
+		System.out.println(segundoNumero);
+		
+	}
+
+}
