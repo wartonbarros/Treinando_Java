3d1323b3a5b33eb7cc07b933c71de99a67c102fc
Warton Barros <warton86@gmail.com>
2020-10-20 06:57:13 -0300

    Somatoria

---
+++ sintaxe-variaveis-e-fluxo/src/TestaWhile.java
@@ -0,0 +1,20 @@
+
+public class TestaWhile {
+	
+	public static void main(String[] args) {
+		
+		int contador = 0;
+		while(contador <= 10) {
+		//while recebe sempre uma expressão booleana
+			System.out.println(contador);
+			
+			//contador = contador + 1;
+			//contador ++;
+			contador += 1;
+			
+		}
+		System.out.println(contador);
+		
+	}
+
+}
